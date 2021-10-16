9d05c6d006a379ef8eac4d2dd50e4d09d66a0ee8
jiminsu <4leaf.edu.jmsu@gmail.com>
2021-10-16 23:05:30 +0900

    a

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const globalRouter = require("./routers/globalRouter");
 const path = require("path");
 
 const app = express();
