88f325553ea9201b72807ba6b64012bad5e0e348
jiminsu <4leaf.edu.jmsu@gmail.com>
2021-10-16 22:46:16 +0900

    a

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 const express = require("express");
 const morgan = require("morgan");
+
+const app = express();
+const PORT = 4000;
+
+app.set("view engine", "pug");
+app.use(morgan(`dev`));
+
+app.listen(PORT, ()=>{
+    console.log("SERVER START");
+});
