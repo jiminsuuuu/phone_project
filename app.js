7bc50df5e8947737fe7e4c11a8ec95e18d6103ab
jiminsu <4leaf.edu.jmsu@gmail.com>
2021-10-16 22:57:51 +0900

    a

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const morgan = require("morgan");
+const path = require("path");
 
 const app = express();
 const PORT = 4000;
 
 app.set("view engine", "pug");
 app.use(morgan(`dev`));
+
+app.use("/assets", express.static(path.join(__dirname,"/assets")));
 
 app.listen(PORT, ()=>{
     console.log("SERVER START");
