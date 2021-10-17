e494edf22d954f5d414c2439da2f3d8714294d9d
jms3374 <jiminseung450@gmail.com>
2021-10-17 09:57:22 +0900

    a

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -6,6 +6,17 @@ router.get("/", (req,res)=>{
     res.render("screens/main");
 });
 
+router.get("/phone", (req, res) => {
+    res.render("screens/phone")
+});
+
+router.get("/computer", (req, res) => {
+    res.render("screens/computer")
+});
+
+router.get("/login", (req, res) => {
+    res.render("screens/login")
+});
 
 
 module.exports = router;
